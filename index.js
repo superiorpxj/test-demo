302f510968bba1a7e9ebb51f3080b274802dcb63
五月的夏天 <568378307@qq.com>
2023-04-20 13:50:25 +0800

    再提交一次

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ window.onload = function(){
         alert('这是测试代码');
     })
 }
+window.onload = function(){
+    var box = document.getElementsByClassName('box');
+    box.addEventListener('click',
+    ()=>{
+        alert('代码');
+    })
+}
